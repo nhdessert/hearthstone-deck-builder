c1bfbd63db2ee61160e9c22622ece5c9feeb21dd
Nate Dessert <nhdessert@gmail.com>
2015-01-07 20:38:54 -0500

    Initial project commit

---
+++ HearthstoneDeckBuilder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HearthstoneDeckBuilder.MvcApplication" Language="C#" %>
---
+++ HearthstoneDeckBuilder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HearthstoneDeckBuilder.Startup))]
+namespace HearthstoneDeckBuilder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
